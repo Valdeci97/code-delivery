3ded9125fbc8dce6f050e7768467597708932de1
valdeci <moura97.valdeci@gmail.com>
2022-10-22 00:04:27 -0300

    build(kafka): create kafka configuration and test its connection

--- simulator/main.go
+++ simulator/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
-	"github.com/Valdeci97/full-cycle-simulator/application/route"
+	"github.com/Valdeci97/full-cycle-simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	r := route.Route{
-		ID:       "1",
-		ClientID: "1",
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
 	}
-	r.LoadPositions()
-	stringjson, _ := r.ExportJsonPositions()
-	fmt.Println(stringjson[0])
+}
+
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+	producer := kafka.NewKafkaProducer()
+	kafka.Publish("olá kafka", "readtest", producer)
+
+	for {
+		_ = 1
+	}
+
+	// r := route.Route{
+	// 	ID:       "1",
+	// 	ClientID: "1",
+	// }
+	// r.LoadPositions()
+	// stringjson, _ := r.ExportJsonPositions()
+	// fmt.Println(stringjson[0])
 }
