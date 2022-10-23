91840787423d3b5f889ec401f550f09eba988c41
valdeci <moura97.valdeci@gmail.com>
2022-10-22 22:02:26 -0300

    feat(app): connect kafka

--- simulator/main.go
+++ simulator/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	kafka2 "github.com/Valdeci97/full-cycle-simulator/application/kafka"
 	"github.com/Valdeci97/full-cycle-simulator/infra/kafka"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/joho/godotenv"
@@ -19,18 +21,8 @@ func main() {
 	msgChan := make(chan *ckafka.Message)
 	consumer := kafka.NewKafkaConsumer(msgChan)
 	go consumer.Consume()
-	producer := kafka.NewKafkaProducer()
-	kafka.Publish("olá kafka", "readtest", producer)
-
-	for {
-		_ = 1
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg) // criação de multi threads
 	}
-
-	// r := route.Route{
-	// 	ID:       "1",
-	// 	ClientID: "1",
-	// }
-	// r.LoadPositions()
-	// stringjson, _ := r.ExportJsonPositions()
-	// fmt.Println(stringjson[0])
 }
