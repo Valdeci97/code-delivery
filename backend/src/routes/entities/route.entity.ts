585b588b92c5ae43caf80c63a4523abaf67e7fd7
valdeci <moura97.valdeci@gmail.com>
2022-10-27 10:36:17 -0300

    feat(route): create route entity schema

--- backend/src/routes/entities/route.entity.ts
+++ backend/src/routes/entities/route.entity.ts
@@ -1,1 +1,30 @@
-export class Route {}
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type RouteDocument = Route & Document;
+
+type Position = { lat: number; long: number };
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string;
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      long: { type: Number },
+    }),
+  )
+  startPosition: Position;
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      long: { type: Number },
+    }),
+  )
+  endPosition: Position;
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route);
